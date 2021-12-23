69bbba391c686a486b2a21abfc1708d961e8f07b
Bloo00 <kabin.yangg@gmail.com>
2021-12-23 13:58:06 -0800

    working data showing on the json on webpage

---
+++ models/seasons.js
@@ -0,0 +1,14 @@
+// first collection schema
+//season schema
+const mongoose = require("mongoose");
+
+const seasonSchema = new mongoose.Schema({
+    url: String,
+    title: { type: String, requried: true },
+    image_url: String,
+    synopsis: String,
+});
+
+const Season = mongoose.model("Season", seasonSchema);
+
+module.exports = Season;
